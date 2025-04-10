949ac30a3ecfd4a8f200830893a36022352ca985
Arunava-27062003 <arunava.kundu@iemlabs.com>
2025-04-10 11:30:18 +0530

    just basics

--- javascript-examples/play.js
+++ javascript-examples/play.js
@@ -7,18 +7,18 @@ console.log(newNumbers);
 
 // rest operator
 const filter = (...args) => {
-    return args.filter(el => el === 1);
-    }
+  return args.filter((el) => el === 1);
+};
 console.log(filter(1, 2, 3));
 
 // destructuring
 const person = {
-    name: 'Max'
+  name: "Max",
 };
 
 const newPerson = {
-    ...person,
-    age: 28
+  ...person,
+  age: 28,
 };
 
 console.log(newPerson);
@@ -30,24 +30,23 @@ console.log(num1, num3);
 
 // reference and primitive types
 const user = {
-    name: 'Max'
+  name: "Max",
 };
 
 const secondUser = user;
-user.name = 'Manu';
+user.name = "Manu";
 console.log(secondUser);
-
 
 // spread operator
 const people = {
-    name: 'Max'
+  name: "Max",
 };
 
 const secondPeople = {
-    ...people
+  ...people,
 };
 
-people.name = 'Manu';
+people.name = "Manu";
 
 console.log(secondPeople);
 
@@ -55,47 +54,71 @@ console.log(secondPeople);
 // map
 const numso = [1, 2, 3];
 const doubleNumArray = numbers.map((num) => {
-    return num * 2;
+  return num * 2;
 });
 
 console.log(numbers);
 
 // filter
-const filterNumArray = numbers.filter(num => {
-    return num === 1;
+const filterNumArray = numbers.filter((num) => {
+  return num === 1;
 });
 
 console.log(filterNumArray);
 
 // reduce
 const reduceNumArray = numbers.reduce((curResult, curValue) => {
-    return curResult * curValue;
+  return curResult * curValue;
 }, 1);
 
 console.log(reduceNumArray);
 
 // find
 
-const findNumArray = numbers.find(num => {
-    return num === 1;
+const findNumArray = numbers.find((num) => {
+  return num === 1;
 });
 
 console.log(findNumArray);
 
 // findIndex
-const findIndexNumArray = numbers.findIndex(num => {
-    return num === 1;
+const findIndexNumArray = numbers.findIndex((num) => {
+  return num === 1;
 });
 
 console.log(findIndexNumArray);
 
 // chaining
 const chainNumArray = numbers
-    .map(num => {
-        return num * 2;
-    })
-    .filter(num => {
-        return num === 2;
-    });
+  .map((num) => {
+    return num * 2;
+  })
+  .filter((num) => {
+    return num === 2;
+  });
 
 console.log(chainNumArray);
+
+// async code
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, 1500);
+  });
+  return promise;
+};
+
+setTimeout(() => {
+  console.log("Timer is done!");
+  fetchData()
+    .then((text) => {
+      console.log(text);
+      return fetchData();
+    })
+    .then((text2) => {
+      console.log(text2+"2");
+    });
+}, 2000);
+console.log("Hello!");
+console.log("Hi!");
