2b974e2cd28b43b80ab9af90bc3295d296af8149
Arunava-27 <drarunkundu22@gmail.com>
2026-02-25 13:58:48 +0530

    feat: implement route for adding products and streamline middleware usage

--- working-with-expressjs/app.js
+++ working-with-expressjs/app.js
@@ -4,18 +4,17 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => { // Middleware
-    console.log("In Middleware"); 
-    next();   // to move to the next middleware or route handler
+app.use('/add-product', (req, res) => {
+    console.log("add product");
+    
+    res.send("<h1>Add Your Product</h1>")
 })
 
-
-app.use((req, res, next) => { // Another Middleware
-    console.log("In another Middleware"); 
+app.use('/', (req, res) => { // Another Middleware
+    console.log("root path / home route"); 
     
     res.send("<h1>Hello from Express</h1>")   // to send response to the client
 })
 
-const server = http.createServer(app)
 
-server.listen(5000)
+app.listen(5000)
