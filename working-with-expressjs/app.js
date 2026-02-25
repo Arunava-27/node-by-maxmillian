e940386ad64458ea00a5fc817548ddbd898d0f79
Arunava-27 <drarunkundu22@gmail.com>
2026-02-25 12:22:39 +0530

    feat: add middleware for logging requests and sending response in Express app

--- working-with-expressjs/app.js
+++ working-with-expressjs/app.js
@@ -4,6 +4,18 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next) => { // Middleware
+    console.log("In Middleware"); 
+    next();   // to move to the next middleware or route handler
+})
+
+
+app.use((req, res, next) => { // Another Middleware
+    console.log("In another Middleware"); 
+    
+    res.send("<h1>Hello from Express</h1>")   // to send response to the client
+})
+
 const server = http.createServer(app)
 
 server.listen(5000)
