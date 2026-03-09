4cffe8fd082ed0b486064c7400c3078f852af263
Arunava-27 <drarunkundu22@gmail.com>
2026-03-09 12:13:51 +0530

    feat: serve HTML views for add-product and shop routes

--- working-with-expressjs/routes/shop.js
+++ working-with-expressjs/routes/shop.js
@@ -1,11 +1,10 @@
-const express = require('express')
+const path = require("path");
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => { 
-    console.log("root path / home route"); 
-    
-    res.send("<h1>Hello from Express</h1>")   // to send response to the client
-})
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+});
 
 module.exports = router;
