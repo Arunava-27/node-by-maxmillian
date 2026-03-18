2f1feb6339d7378c2c6629a32b86b2cff09141ed
Arunava-27 <drarunkundu22@gmail.com>
2026-03-18 14:45:36 +0530

    feat: add CSS styles for product and main pages, refactor HTML to link external stylesheets

--- working-with-expressjs/app.js
+++ working-with-expressjs/app.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
-const { lstat } = require("fs");
+
 
 const app = express()
 
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes);
 
